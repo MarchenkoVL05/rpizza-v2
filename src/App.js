0efc0bf8cebeb46fed87d1211660a81a4526d51e
Vladimir <BITcheat@mail.ru>
2022-06-28 21:45:19 +0500

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import './App.css';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import PizzaBlock from './components/PizzaBlock';
+
+import './scss/app.scss';
+import Sort from './components/Sort';
 
 function App() {
   return (
-    <div>Hello</div>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
