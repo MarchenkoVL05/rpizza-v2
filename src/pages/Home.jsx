db73d41aa13d585c831b5ad63a57ecf28aff5465
Vladimir <BITcheat@mail.ru>
2022-07-01 03:08:17 +0500

    Cart page + adaptive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,21 +6,22 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 import Sort from '../components/Sort';
 
 function Home() {
-    const [pizzas, setPizzas] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
-  
-    React.useEffect(() => {
-      fetch('https://62bdba87bac21839b609fc45.mockapi.io/pizzas')
-        .then((res) => {
-          return res.json();
-        })
-        .then((items) => {
-          setPizzas(items);
-          setIsLoading(false);
-        });
-    }, []);
-  
-    return (
+  const [pizzas, setPizzas] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://62bdba87bac21839b609fc45.mockapi.io/pizzas')
+      .then((res) => {
+        return res.json();
+      })
+      .then((items) => {
+        setPizzas(items);
+        setIsLoading(false);
+      });
+      window.scrollTo(0, 0);
+  }, []);
+
+  return (
     <div>
       <div className="content__top">
         <Categories />
@@ -29,11 +30,11 @@ function Home() {
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
         {isLoading
-            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-            : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
